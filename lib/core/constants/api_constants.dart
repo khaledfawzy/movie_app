be106910591603bf9d11642f63c27c3e7250bffb
mokh <mahmouddado555@outlook.com>
2024-09-16 03:13:55 +0300

    adding api services and constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,11 @@
+const String apiKey =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YjcyNjk3M2JhNDY5ODA4MDY2NGM0N2JjNTYwMmUyZCIsIm5iZiI6MTcyNjQ0MzQxMy42NDAwNDIsInN1YiI6IjY1MTJlNjdjYTkxMTdmNzY1ZTBjYzc1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RVN0jwvxBsKimJVRNIPSIeCrx-dBEg9TOnmtQD1Qz4Q';
+const String baseUrl = 'https://api.themoviedb.org/3/';
+const String popularMovies = 'movie/popular?';
+const String upcomingMovies = 'movie/upcoming?';
+const String topRatedMovies = 'movie/top_rated?';
+const String movieDetails = 'movie/';
+const String similarMovies = '/similar?';
+const String searchMovies = 'search/movie?query=';
+const String moviesCategoryNames = 'genre/movie/list?';
+const String moviesCategory = 'discover/movie?with_genres=';
---
+++ lib/core/services/network/api_services.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:movie_app/core/constants/api_constants.dart';
+  
+class ApiServices {
+  final Dio _dio;
+
+  Future<List<dynamic>> get({
+    required String endPoint,
+    Map<String, dynamic>? parameters,
+    bool isListString=false,
+  }) async {
+    Response response = await _dio.get(
+      '$baseUrl$endPoint',
+      queryParameters: parameters,
+      options:  Options(headers: {
+              'Authorization': 'Bearer $apiKey',
+            }),
+    );
+    if (isListString) {
+      List<String> stringList = response.data.map((e) => e.toString()).toList();
+      return stringList;
+    } else {
+      return response.data;
+    }
+  }
+
+  ApiServices(this._dio);
+}
