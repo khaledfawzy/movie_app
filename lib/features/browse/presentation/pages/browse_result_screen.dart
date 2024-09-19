f385ba83abca41e8167698e3b7791f2328d60a3c
mokh <mahmouddado555@outlook.com>
2024-09-19 03:45:46 +0300

    "Added navigation to MovieDetailsScreen from BrowseResultItem and removed unnecessary const keywords"

--- lib/features/browse/presentation/pages/browse_result_screen.dart
+++ lib/features/browse/presentation/pages/browse_result_screen.dart
@@ -6,6 +6,7 @@ import 'package:movie_app/features/browse/data/repos/browse_repo.dart';
 import 'package:movie_app/features/browse/presentation/manager/genre_cubit/genre_cubit.dart';
 import 'package:movie_app/features/browse/presentation/widgets/browse_result_item.dart';
 import 'package:movie_app/features/home/data/models/movie_model.dart';
+import 'package:movie_app/features/movie_details/presentation/pages/movie_details_screen.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 class BrowseResultScreen extends StatelessWidget {
@@ -26,21 +27,33 @@ class BrowseResultScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(height: 16.0),
-              const Text(
-                'Movie List',
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 16.0),
+                const Text(
+                  'Movie List',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16.0),
                 Expanded(
                   child: BlocBuilder<GenreCubit, GenreState>(
                     builder: (context, state) {
                       if (state is GenreSuccessState) {
                         return ListView.separated(
-                            separatorBuilder: (context, index) => const Divider(),
+                            separatorBuilder: (context, index) =>
+                                const Divider(),
                             itemCount: state.genreList.length,
                             itemBuilder: (context, index) {
-                              return BrowseResultItem(
-                                movieModel: state.genreList[index],
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => MovieDetailsScreen(
+                                          movieId: state.genreList[index].id!),
+                                    ),
+                                  );
+                                },
+                                child: BrowseResultItem(
+                                  movieModel: state.genreList[index],
+                                ),
                               );
                             });
                       } else if (state is GenreFailureState) {
