a8a6d915d112f21308a90bb7f0c39a6097dca831
kareem elshaar <kemo.elshaar22@gmail.com>
2024-09-19 05:30:19 +0300

    movie_app

--- lib/core/services/network/api_services.dart
+++ lib/core/services/network/api_services.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+ import 'package:dio/dio.dart';
 import 'package:movie_app/core/constants/api_constants.dart';
   
 class ApiServices {
@@ -45,3 +45,9 @@ class ApiServices {
 
   ApiServices(this._dio);
 }
+/*
+original title
+overview
+genres
+
+* */
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movie_app/core/constants/constants.dart';
 import 'package:movie_app/core/utils/setup_serv_locator.dart';
 import 'package:movie_app/core/utils/simple_bloc_observer.dart';
@@ -19,6 +18,5 @@ void main() async {
   setupServicesLocator();
   Bloc.observer = SimpleBlocObserver();
   await Future.delayed(const Duration(seconds: 3));
-  FlutterNativeSplash.remove();
-  runApp(const MovieApp());
+   runApp(const MovieApp());
 }
